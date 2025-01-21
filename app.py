4a5f36d2525234c6ecb381825fe7dd29c4752c07
thanhnew2001 <thanh.hispvietnam@gmail.com>
2025-01-21 11:57:40 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 import os
+import string
+import unicodedata
+from datetime import datetime
+from pprint import pprint
+
+from tqdm import tqdm
 import torch
 import torchaudio
 from flask import Flask, render_template, request, jsonify, send_from_directory
 from werkzeug.utils import secure_filename
 from underthesea import sent_tokenize
-from datetime import datetime
 from unidecode import unidecode
 from vinorm import TTSnorm
 from TTS.tts.configs.xtts_config import XttsConfig
 from TTS.tts.models.xtts import Xtts
+
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -29,6 +35,10 @@ vixtts_model = None
 def clear_gpu_cache():
     if torch.cuda.is_available():
         torch.cuda.empty_cache()
+
+# Check if the uploaded file has an allowed extension
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def load_model(xtts_checkpoint, xtts_config, xtts_vocab):
     clear_gpu_cache()
@@ -70,10 +80,46 @@ def get_file_name(text, max_char=50):
     filename = f"{current_datetime}_{filename}"
     return filename
 
-# Run TTS function
-def run_tts(XTTS_MODEL, lang, tts_text, speaker_audio_file, normalize_text=True):
+def calculate_keep_len(text, lang):
+    if lang in ["ja", "zh-cn"]:
+        return -1
+
+    word_count = len(text.split())
+    num_punct = (
+        text.count(".")
+        + text.count("!")
+        + text.count("?")
+        + text.count(",")
+    )
+
+    if word_count < 5:
+        return 15000 * word_count + 2000 * num_punct
+    elif word_count < 10:
+        return 13000 * word_count + 2000 * num_punct
+    return -1
+
+def run_tts(XTTS_MODEL, lang, tts_text, speaker_audio_file,
+            normalize_text= True,
+            verbose=False,
+            output_chunks=False):
+    """
+    Run text-to-speech (TTS) synthesis using the provided XTTS_MODEL.
+
+    Args:
+        XTTS_MODEL: A pre-trained TTS model.
+        lang (str): The language of the input text.
+        tts_text (str): The text to be synthesized into speech.
+        speaker_audio_file (str): Path to the audio file of the speaker to condition the synthesis on.
+        normalize_text (bool, optional): Whether to normalize the input text. Defaults to True.
+        verbose (bool, optional): Whether to print verbose information. Defaults to False.
+        output_chunks (bool, optional): Whether to save synthesized speech chunks separately. Defaults to False.
+
+    Returns:
+        str: Path to the synthesized audio file.
+    """
+
     if XTTS_MODEL is None or not speaker_audio_file:
-        return "Error with model or speaker audio", None
+        return "You need to run the previous step to load the model !!", None, None
 
     output_dir = "./output"
     os.makedirs(output_dir, exist_ok=True)
@@ -85,14 +131,24 @@ def run_tts(XTTS_MODEL, lang, tts_text, speaker_audio_file, normalize_text=True)
         sound_norm_refs=XTTS_MODEL.config.sound_norm_refs,
     )
 
-    # Normalize text if needed
     if normalize_text and lang == "vi":
-        tts_text = normalize_vietnamese_text(tts_text)
+        # Bug on google colab
+        try:
+            tts_text = normalize_vietnamese_text(tts_text)
+        except:
+            print("Error normalize")
 
-    tts_texts = sent_tokenize(tts_text)
+    if lang in ["ja", "zh-cn"]:
+        tts_texts = tts_text.split("。")
+    else:
+        tts_texts = sent_tokenize(tts_text)
+
+    if verbose:
+        print("Text for TTS:")
+        print(tts_texts)
+
     wav_chunks = []
-    
-    for text in tts_texts:
+    for text in tqdm(tts_texts):
         if text.strip() == "":
             continue
 
@@ -108,11 +164,25 @@ def run_tts(XTTS_MODEL, lang, tts_text, speaker_audio_file, normalize_text=True)
             top_p=0.85,
         )
 
+        # Quick hack for short sentences
+        keep_len = calculate_keep_len(text, lang)
+        wav_chunk["wav"] = torch.tensor(wav_chunk["wav"][:keep_len])
+
+        if output_chunks:
+            out_path = os.path.join(output_dir, f"{get_file_name(text)}.wav")
+            torchaudio.save(out_path, wav_chunk["wav"].unsqueeze(0), 24000)
+            if verbose:
+                print(f"Saved chunk to {out_path}")
+
         wav_chunks.append(wav_chunk["wav"])
 
     out_wav = torch.cat(wav_chunks, dim=0).unsqueeze(0)
     out_path = os.path.join(output_dir, f"{get_file_name(tts_text)}.wav")
     torchaudio.save(out_path, out_wav, 24000)
+
+    if verbose:
+        print(f"Saved final file to {out_path}")
+
     return out_path
 
 # Flask routes
@@ -122,6 +192,8 @@ def index():
 
 @app.route('/process', methods=['POST'])
 def process():
+    global vixtts_model  # Declare the variable as global so we can modify it
+    
     input_text = request.form['input_text']
     language = request.form['language']
     normalize_text = 'normalize_text' in request.form
@@ -139,11 +211,10 @@ def process():
 
         if vixtts_model is None:
             # If model is not loaded, load it here
-            global vixtts_model
             vixtts_model = load_model('model/model.pth', 'model/config.json', 'model/vocab.json')
 
         # Run TTS
-        audio_file = run_tts(vixtts_model, language, input_text, file_path, normalize_text)
+        audio_file = run_tts(vixtts_model, "vi", input_text, file_path, normalize_text)
 
         # Return the URL for the generated audio file
         return jsonify({'audio_file': f'/download/{os.path.basename(audio_file)}'})
